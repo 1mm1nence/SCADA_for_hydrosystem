dc878b8c0aa8adf9809176fd19aa42c88fb9b4e8
1mm1nence <110335004+1mm1nence@users.noreply.github.com>
2023-05-24 21:23:26 +0300

    Added needed url path

--- mysite/opcuaAPI/urls.py
+++ mysite/opcuaAPI/urls.py
@@ -5,4 +5,5 @@ app_name = 'opcuaAPI'
 
 urlpatterns = [
     path('post', views.GetData.as_view(), name='get_from_device'),
+    path('get_data_from_server', views.ShareData.as_view(), name='get_from_server')
 ]
